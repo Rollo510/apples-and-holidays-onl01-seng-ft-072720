7422cb252fcd77a3758eadb7d1cd1fc5061957c1
Kevin Britt <kbritt510@gmail.com>
2020-08-03 18:22:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,14 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  
+  
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s.capitalize}:"
+    holiday.each do |event, supplies|
+      puts "#{event.to_s.split("_").map do {|x| x.capitalize}"
+    end
+  end
 end
 
 
