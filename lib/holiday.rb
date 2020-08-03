59cc6f0fcd53fd15a695a994c85186964b1575ba
Kevin Britt <kbritt510@gmail.com>
2020-08-03 18:36:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,10 +92,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash.map do |hash|
-    hash.select do |key, value|
-      [:winter, :summer, :fall, :spring].include? value
-      return
+
      end 
   end
 end
